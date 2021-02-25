c034be95b879a5fdb6c777b5a6099979c322c733
joannsiciliano <joannsiciliano@gmail.com>
2021-02-25 13:30:55 -0700

    edit JS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 =document.createElement('h2');
-h2.textContent=":)";
+h2.textContent="";
 document.querySelector('body').appendChild(h2);
+document.body.style.cursor="crosshair";
+let stats= document.getElementsById("stats");
+stats.addEventListener("mouseover",function(event){
+    event.target.style.color="purple"
+});
