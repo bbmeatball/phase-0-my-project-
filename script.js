2a90aa9dc6ce74042ba9f15731ed61686c52940b
joannsiciliano <joannsiciliano@gmail.com>
2021-03-06 18:03:29 -0700

    remove mouseevent

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const h2 =document.createElement('h2');
 h2.textContent="";
 document.querySelector('body').appendChild(h2);
 document.body.style.cursor="crosshair";
-let stats= document.getElementsById("stats");
-stats.addEventListener("mouseover",function(event){
-    event.target.style.color="purple"
+let colorHover= document.getElementsByTagName('h1')
+colorHover.addEventListener("mouseenter",function(event){
+    event.target.style.color="purple";
 });
